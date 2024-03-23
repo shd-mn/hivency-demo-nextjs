b15554c8566ce1cdd8180b8e84e8b0fb5b5bd3a3
Shadman <shadman.alizada@gmail.com>
2024-03-23 23:39:57 +0400

    feat: created custom Button components

---
+++ components/UI/SwipeBtn.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+type PropTypes = {
+  text: string;
+};
+
+function SwipeBtn({ text }: PropTypes) {
+  return (
+    <div>
+      <Link
+        href="#"
+        className={`group block h-[60px] w-28 overflow-hidden  rounded-full py-4 font-outfit text-lg font-medium duration-500`}
+      >
+        <div className="relative h-full w-full overflow-hidden ">
+          <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 whitespace-nowrap duration-300 group-hover:-translate-y-10">
+            {text}
+          </span>
+          <span className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 translate-y-10 whitespace-nowrap duration-300 group-hover:-translate-y-1/2">
+            {text}
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default SwipeBtn;
