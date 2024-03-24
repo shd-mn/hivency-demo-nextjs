976d63cd233095bcd005e25617c1f3c60578e7c1
Shadman <shadman.alizada@gmail.com>
2024-03-24 19:49:25 +0400

    feat: Our FAQ section created

---
+++ container/Home/FAQSection.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+import SubTitle from "./components/SubTitle";
+import Title from "./components/Title";
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import bgImg from "/public/assets/images/testimonial.jpg";
+import bgImg from "@/public/assets/images/testimonial.jpg";
+import Accordion from "@/components/UI/Accordion";
+import { faqData } from "./data/faqData";
+
+function FAQSection() {
+  return (
+    <section
+      className="relative h-full w-full bg-g-light2 bg-scroll py-[106px] after:absolute after:left-0 after:top-0 after:z-10 after:h-full after:w-full after:bg-black-100/5 after:content-['']"
+      style={{
+        backgroundImage: `url('/assets/images/testimonial.jpg')`,
+      }}
+    >
+      <div className="container relative z-20">
+        <header className="mb-12 flex w-full flex-col items-center justify-center px-80">
+          <SubTitle title="Our FAQ" className="mb-5" />
+          <Title
+            part1="Frequently asked "
+            underLine="goals"
+            className="mb-10 text-center font-outfit text-[50px] font-medium leading-[60px] tracking-tight"
+          />
+          <p className="text-center text-lg">
+            Nemo enim ipsam voluptatem quia voluptas sit asper natur aut oditut
+            fugit Nemo enim ipsam voluptatem .
+          </p>
+        </header>
+
+        <div className="grid grid-cols-2 items-start gap-x-8 gap-y-5">
+          {faqData.map((item, idx) => (
+            <Accordion key={idx} item={item} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default FAQSection;
---
+++ container/Home/data/faqData.ts
@@ -0,0 +1,37 @@
+export interface FaqDataType {
+  question: string;
+  answer: string;
+}
+
+export const faqData: FaqDataType[] = [
+  {
+    question: "1. Accusantium Doloremque Laudantium Totam Rem?",
+    answer:
+      "Ut enim ad minima veniam, quis nostrum exercitation em ullam corporis suscipit laboriosam, nisi ut al",
+  },
+  {
+    question: "2. Voluptatem Quia Voluptas Sit Aspernatur Aut Odit Aut?",
+    answer:
+      "Ut enim ad minima veniam, quis nostrum exercitation em ullam corporis suscipit laboriosam, nisi ut al",
+  },
+  {
+    question: "3. Accusantium Doloremque Laudantium Totam Rem?",
+    answer:
+      "Ut enim ad minima veniam, quis nostrum exercitation em ullam corporis suscipit laboriosam, nisi ut al",
+  },
+  {
+    question: "4. Natus Error Sit Voluptatem Accusantium Doloremque?",
+    answer:
+      "Ut enim ad minima veniam, quis nostrum exercitation em ullam corporis suscipit laboriosam, nisi ut al",
+  },
+  {
+    question: "5. Dolores Eos Qui Ratione Voluptatem Sequi Nesciunt?",
+    answer:
+      "Ut enim ad minima veniam, quis nostrum exercitation em ullam corporis suscipit laboriosam, nisi ut al",
+  },
+  {
+    question: "6. Ut enim ad minim veniam, quis nostrud exercitation?",
+    answer:
+      "Ut enim ad minima veniam, quis nostrum exercitation em ullam corporis suscipit laboriosam, nisi ut al",
+  },
+];
