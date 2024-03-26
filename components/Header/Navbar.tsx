fa4f777c082652a41a07d3e799c63a20fa991730
Shadman <shadman.alizada@gmail.com>
2024-03-26 23:06:02 +0400

    some changes

--- components/Header/Navbar.tsx
+++ components/Header/Navbar.tsx
@@ -5,32 +5,32 @@ function Navbar() {
     <nav>
       <ul className="flex items-center text-lg">
         <li>
-          <Link href="/" className="px-5">
+          <Link href="/" className="px-5 hover:text-blue-700">
             Home
           </Link>
         </li>
         <li>
-          <Link href="/about" className="px-5">
+          <Link href="/about" className="px-5 hover:text-blue-700">
             About
           </Link>
         </li>
         <li>
-          <Link href="/services" className="px-5">
+          <Link href="/services" className="px-5 hover:text-blue-700">
             Services
           </Link>
         </li>
         <li>
-          <Link href="/projects" className="px-5">
+          <Link href="/projects" className="px-5 hover:text-blue-700">
             Projects
           </Link>
         </li>
         <li>
-          <Link href="/blog" className="px-5">
+          <Link href="/blog" className="px-5 hover:text-blue-700">
             Blog
           </Link>
         </li>
         <li>
-          <Link href="/contact" className="px-5">
+          <Link href="/contact" className="px-5 hover:text-blue-700">
             Contact
           </Link>
         </li>
